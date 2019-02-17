3452e0c5acf42f003e0f3f781484558a21519eb3
fedrius <paulmlee7@gmail.com>
2019-02-16 20:53:22 -0800

    converted stateless widget to a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 // Main widget tree
-class MyApp extends StatelessWidget {
-  @override // overriding a method. easier to read for others
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+// underscore should for classes that are only in the same file
+// This state class belongs to the MyApp stateful widget
+class _MyAppState extends State<MyApp> {
+  @override // overriding the build method. easier to read for others
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
@@ -16,20 +26,20 @@ class MyApp extends StatelessWidget {
           ),
           // shows in column format
           body: Column(
-            // an array of cards
+            // an array of btn and cards
             children: [
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                      onPressed: () {},
-                      child: Text('Add Product'),
-                    ),
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
               ),
               Card(
                 child: Column(
                   children: <Widget>[
                     Image.asset('assets/food.jpg'),
-                    Text('Food Paradisee')
+                    Text('Food Paradise')
                   ],
                 ),
               ),
