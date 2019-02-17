ced6699f42c5b2679f66ff16423699318c3d8866
fedrius <paulmlee7@gmail.com>
2019-02-16 20:28:17 -0800

    added a app body with a card widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 
 // Every component is a widget in flutter!
 // alt+shift+f to format code
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 // Main widget tree
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override // overriding a method. easier to read for others
+  Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:AppBar(
+        appBar:AppBar( //Top nav bar
           title: Text('EasyList'),
-        ),
+        ), 
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('Food Paradisee')
+        ],)),
       ),
-    ); //
+    );
   }
 }
